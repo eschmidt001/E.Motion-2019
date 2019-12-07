1539c91f4ac75efc16e6f574f2423173bc2d1b51
Sohwi Jung <sj399@cornell.edu>
2019-12-07 12:10:27 -0500

    Slideshow added.

--- scripts/validations.js
+++ scripts/validations.js
@@ -10,41 +10,6 @@ $(document).ready(function(){
     $("#comment").attr("required", false);
   })
 
-  //slideshow
-  var slideIndex = 1;
-  showSlides(slideIndex);
-
-  function plusSlides(n) {
-    sshowSlides(slideIndex +=n);
-  }
-
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-
-  function showSlides(n){
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex=1}
-    if (n<1) {slideIndex = slides.length}
-    for (i=0; i<slides.length; i++) {slides[i].style.display = "none"}
-    slides[slideIndex-1].style.display = "block"
-  }
-
-  var deadline = new Date("Jan 5, 2018 15:37:25").getTime();
-  var x = setInterval(function() {
-    var now = new Date().getTime();
-    var t = deadline - now;
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((t % (1000 * 60)) / 1000);
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-    if (t < 0) {
-      clearInterval(x);
-      document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-  }, 1000);
 
 
   //form validation
