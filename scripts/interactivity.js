6b2bb5dca170e5e3c234aa11e66bac2e78a02d56
Elisa Schmidt <es797@cornell.edu>
2019-12-09 11:30:55 -0500

    countdown timer

--- scripts/interactivity.js
+++ scripts/interactivity.js
@@ -21,4 +21,19 @@ $(document).ready(function() {
     $("#slideshowImage").attr("src", images[currentIndex]);
   });
 
+  //countdown
+  var countdownDate= new Date("Feb 1, 2020").getTime();
+
+  var x = setInterval(function(){
+
+    var now= new Date().getTime();
+    var distance= countdownDate - now; //total time
+
+    var days = Math.floor(distance / (1000 * 60 * 60 *24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+    document.getElementById("countdown").innerHTML = days + "d" + hours + "h" + minutes + "m" + seconds + "s" + " until applications open! "+ "yeet";
+  });
 });
